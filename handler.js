ecf85af7a11f7bc2936ce53bb4fa7ba4b27503bb
choi0926 <ghtjd753753@gmail.com>
2020-05-12 17:19:52 +0900

    serverless, lambda : apollo-server-lambda

---
+++ handler.js
@@ -0,0 +1,21 @@
+const { ApolloServer, gql } = require('apollo-server-lambda');
+ 
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+ 
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  playground: true,
+  introspection: true,
+});
+ 
+exports.graphqlHandler = server.createHandler();
